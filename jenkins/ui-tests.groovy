952907794f8c6fff395d3e66192e8a781931d730
Tretjakoff <databank94@gmail.com>
2024-07-24 13:43:16 +0300

    add groovy file

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -1,25 +1,25 @@
-
-
 timeout(60) {
     node("maven") {
         def testContainerName = "ui_tests_$BUILD_NUMBER"
-
-        prepareConfig()
         try {
             wrap([$class: 'BuildUser']) {
                 currentBuild.description = "User: $BUILD_USER"
             }
 
             stage("Run tests ${jobDescription}") {
-            sh "docker run --rm --network=host --name $testContainerName - v $pwd/allure-results:/hhome/ubuntu/target/allure-results - t ui-tests"
+            sh """
+                    docker run --rm --network=host 
+                    -e BROWSER_NAME=$BROWSER_NAME 
+                    -e SELENOID_URL=http://localhost:8090/wd/hub 
+                    --name $testContainerName - v $pwd/allure-results:/home/ubuntu/target/allure-results 
+                    -t ui_tests
+                    """
             }
 
             stage("Publish allure results") {
             allure([
                     disabled: true,
                     includeProperties: false,
-                    jdk: '',
-                    properties: [],
                     reportBuildPolicy: 'ALWAYS',
                     results: ["$pwd/allure-results"]
             ])
