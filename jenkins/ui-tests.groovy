90f006d4fd3d7244d82b55068592822a04c82eb1
Tretjakoff <databank94@gmail.com>
2024-07-27 10:07:58 +0300

    add groovy file

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -1,6 +1,9 @@
 pipeline {
     agent {
-        label 'maven'
+        docker {
+            image 'localhost:5005/maven'
+            args '--privileged --env-file .env'
+        }
     }
     environment {
         MAVEN_LOCAL_REPO = "${WORKSPACE}/.m2/repository"
@@ -42,23 +45,10 @@ pipeline {
             steps {
                 script {
                     sh '''
-                        # Запуск Docker контейнера и выполнение команд внутри него
-                        CONTAINER_ID=$(docker run --privileged -d  --env-file .env\
-                            -v ${MAVEN_LOCAL_REPO}:${MAVEN_LOCAL_REPO} \
-                            localhost:5005/maven \
-                            /bin/bash -c "rm -rf ${DOCKER_HOME}/allure-results/* ${DOCKER_HOME}/allure-report/* && \
-                            mvn clean test -Dmaven.repo.local=${MAVEN_LOCAL_REPO} && \
-                            allure generate ${DOCKER_HOME}/allure-results --clean -o ${DOCKER_HOME}/allure-report")
-                        
-                        # Просмотр логов выполнения тестов
-                        docker logs -f $CONTAINER_ID
-
-                        # Копирование содержимого результатов и отчетов из контейнера
-                        docker cp $CONTAINER_ID:/home/ubuntu/ui-test/allure-results/. ${ALLURE_RESULTS}/
-                        docker cp $CONTAINER_ID:/home/ubuntu/ui-test/allure-report/. ${ALLURE_REPORT}/
-
-                        docker stop $CONTAINER_ID || true
-                        docker rm $CONTAINER_ID || true
+                        # Выполнение команд внутри Docker контейнера
+                        rm -rf ${DOCKER_HOME}/allure-results/* ${DOCKER_HOME}/allure-report/*
+                        mvn clean test -Dmaven.repo.local=${MAVEN_LOCAL_REPO}
+                        allure generate ${DOCKER_HOME}/allure-results --clean -o ${DOCKER_HOME}/allure-report
                     '''
                 }
             }
