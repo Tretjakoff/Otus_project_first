5596f154b880ee5f88899e57c54ec78ab0d6c660
Tretjakoff <databank94@gmail.com>
2024-07-29 20:43:31 +0300

    add groovy file

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,45 +1,30 @@
 package driver;
 
-import driver.impl.ChromeWebDriver;
-import driver.impl.IDriver;
-import exceptions.DriverTypeNotSupported;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import listeners.ActionsListener;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.Locale;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory implements IDriverFactory {
 
-  private String browserType = System.getProperty("browser", "chrome").toLowerCase(Locale.ROOT);
+  private String remoteUrl = System.getProperty("remoteUrl", "http://172.26.0.2:4444/wd/hub");
+  private String chromeVersion = System.getProperty("chromeVersion", "123.0");
 
-  @Override
   public WebDriver getDriver() {
+    WebDriver driver = null;
 
-    switch (this.browserType) {
-      case "chrome": {
+    ChromeOptions options = new ChromeOptions();
+    options.setCapability("browserName", "chrome");
+    options.setCapability("browserVersion", chromeVersion);
 
-        WebDriverManager.chromiumdriver().setup();
-
-        IDriver<ChromeOptions> browserSettings = new ChromeWebDriver();
-
-        WebDriver driver = new EventFiringDecorator<>(new ActionsListener())
-            .decorate(new ChromeDriver(browserSettings.getDriverOptions()));
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-
-        return driver;
-      }
-      default:
-        try {
-          throw new DriverTypeNotSupported(this.browserType);
-        } catch (DriverTypeNotSupported ex) {
-          ex.printStackTrace();
-          return null;
-        }
+    try {
+      driver = new RemoteWebDriver(new URL(remoteUrl), options);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException("Invalid remote URL: " + remoteUrl, e);
     }
+
+    return driver;
   }
 }
